2bb2d97faa36f285fee83ed89ebee37e1556279b
mmounib <mmounib@student.1337.ma>
2023-07-07 17:09:52 +0100

    ending contact section

--- app/Components/Contact.tsx
+++ app/Components/Contact.tsx
@@ -1,5 +1,6 @@
 
 import Link from 'next/link'
+import { useState } from 'react'
 import { BsGithub, BsLinkedin } from 'react-icons/bs'
 
 
@@ -9,6 +10,7 @@ import { BsGithub, BsLinkedin } from 'react-icons/bs'
 // 	}
 // ]
 const Contact = () => {
+
 	return (
 	  	<div className="bg-black">
 			<div className="flex w-[1100px] mx-auto min-h-[400px] mt-12">
@@ -16,16 +18,16 @@ const Contact = () => {
 				<div className="flex flex-col w-full h-full">
 					<form className="w-[500px] mx-auto ">
 						<div className="mb-4 ">
-							<label htmlFor="name" className="block font-medium text-gray-400">Name</label>
-							<input type="text" id="name" name="name" required className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-500" />
+							<label htmlFor="name" className="block font-medium text-white">Name</label>
+							<input type="text" id="name" name="name" required className="bg-gray-500 opacity-70 rounded px-3 py-2 w-full focus:outline-none text-white" />
 						</div>
 						<div className="mb-4">
-							<label htmlFor="email" className="block font-medium text-gray-400">Email</label>
-							<input type="email" id="email" name="email" required className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-500" />
+							<label htmlFor="email" className="block font-medium text-white">Email</label>
+							<input type="email" id="email" name="email" required className=" bg-gray-500 opacity-70 rounded px-3 py-2 w-full focus:outline-none text-white" />
 						</div>
 						<div className="mb-4">
-							<label htmlFor="message" className="block font-medium text-gray-400">Message</label>
-							<textarea id="message" name="message" required className="border border-gray-300 rounded px-3 py-2 w-full h-32 resize-none focus:outline-none focus:border-blue-500"></textarea>
+							<label htmlFor="message" className="block font-medium text-white">Message</label>
+							<textarea id="message" name="message" required className="bg-gray-500 opacity-70 rounded px-3 py-2 w-full h-32 resize-none focus:outline-none text-white "></textarea>
 						</div>
 						<div className="">
 							<button type="submit" className="bg-[#E7B10A] text-white py-2 px-4 rounded ">Send Message</button>
@@ -38,11 +40,11 @@ const Contact = () => {
 			<div className="flex flex-col mx-auto w-[1100px] items-center min-h-[90px] my-8">
 				<h3 className='text-white text-2xl'>Or Contact Me Through</h3>
 				<div className="flex gap-4 mt-8 justify-center items-center w-full">
-					<Link href="https://www.linkedin.com/in/mouad-mounib-954418193/" className='text-white flex gap-2 bg-[#E7B10A] py-2 px-6 items-center'>
+					<Link href="https://www.linkedin.com/in/mouad-mounib-954418193/" className='text-white flex gap-2 bg-[#E7B10A] py-2 px-6 items-center shadow-inner'>
 						<BsLinkedin size={20} className='text-white transition-all' />
 						<span>LinkedIn</span>
 					</Link>
-					<Link href="https://github.com/mmounib" className='text-white flex gap-2 bg-[#E7B10A] py-2 px-8 items-center'>
+					<Link href="https://github.com/mmounib" className='text-white flex gap-2 bg-[#E7B10A] py-2 px-8 items-center shadow-inner'>
 						<BsGithub size={20} className=' ' />
 						<span>Github</span>
 					</Link>
